74a79399c3ac4995609be28433f1cc20eeca31cf
Dell <Dell@SIJU>
2025-10-20 16:04:43 +0530

    Changes

---
+++ src/app/Shared/layout/layout.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './layout.html',
+  styleUrls: ['./layout.scss']
+})
+export class Layout {
+
+}
---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Layout } from './Shared/layout/layout';
+
+@Component({
+  selector: 'app-root',
+  imports: [Layout],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('ParishApp');
+}
