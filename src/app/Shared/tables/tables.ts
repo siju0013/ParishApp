867440c8b48876d2040739b24dc5f0196d798215
Dell <Dell@SIJU>
2025-10-20 18:40:04 +0530

    Dashboard Style Modified

---
+++ src/app/Shared/tables/tables.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tables',
+  imports: [],
+  templateUrl: './tables.html',
+  styleUrl: './tables.scss'
+})
+export class Tables {
+
+}
