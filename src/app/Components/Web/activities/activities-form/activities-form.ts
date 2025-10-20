96d6244de97fef2ba92ce262c747c47f5e1d1c36
Dell <Dell@SIJU>
2025-10-20 23:41:36 +0530

    Activities form

--- src/app/Components/Web/activities/activities-form/activities-form.ts
+++ src/app/Components/Web/activities/activities-form/activities-form.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-activities-form',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './activities-form.html',
   styleUrl: './activities-form.scss'
 })
 export class ActivitiesForm {
+  postForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.postForm = this.fb.group({
+      file: [null, Validators.required],
+      title: ['', Validators.required],
+      description: ['', [Validators.required, Validators.maxLength(150)]],
+      postingDate: ['', Validators.required],
+    });
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.postForm.patchValue({ file });
+    }
+  }
+
+  onSubmit() {
+    if (this.postForm.valid) {
+      console.log('Form Value:', this.postForm.value);
+      // Handle form submission logic here
+    }
+  }
 }
